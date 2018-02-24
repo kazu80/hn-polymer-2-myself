33c6065cc9bc4602e751e234568a069ecbb87553
Kazuyoshi Kawakami <saruyama.monki@gmail.com>
2018-02-24 09:51:29 +0900

    sw precache config

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,9 +1,40 @@
+const path = 'https://localhost';
 module.exports = {
     staticFileGlobs : [
         '/index.html',
         '/manifest.json',
         '/node_modules/@webcomponents/webcomponentsjs/webcomponents*.js',
         '/node_modules/@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js'
+    ],
+    runtimeCaching  : [
+        {
+            urlPattern: new RegExp (`${path}/(news|newest|ask|show|jobs)`),
+            handler   : 'networkFirst',
+            options   : {
+                cache: {
+                    maxEntries: 30,
+                    name      : 'articles-cache'
+                }
+            }
+        }, {
+            urlPattern: new RegExp (`${path}/item/`),
+            handler   : 'networkFirst',
+            options   : {
+                cache: {
+                    maxEntries: 30,
+                    name      : 'comments-cache'
+                }
+            }
+        }, {
+            urlPattern: new RegExp (`${path}/user/`),
+            handler   : 'networkFirst',
+            options   : {
+                cache: {
+                    maxEntries: 30,
+                    name      : 'user-cache'
+                }
+            }
+        }
     ],
     navigateFallback: 'index.html',
 };
